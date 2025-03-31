f1570d18640eb14acaf3ef9847757717538085dc
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2025-03-31 19:51:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "27823209"))
 API_HASH = environ.get("API_HASH", "1d693fcf3bfea119ca1d9057b08a4495")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7963199045:AAF56QGnfSRVlxz-Oh3O02Geo-cJ_hCUxGM")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER_ID = int(environ.get("OWNER_ID", "6004928770"))
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002541046049"))
 MONGO_DB_URI = environ.get("MONGO_DB_URI", "mongodb+srv://kalitgstringsession:kalitgstringsession@cluster0.rby7z6u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
