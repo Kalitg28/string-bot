109c218ce7fd504044e29e500a3a520b49e7ccd6
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2025-03-31 19:49:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER_ID = int(environ.get("OWNER_ID", ""))
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
+API_ID = int(environ.get("API_ID", "27823209"))
+API_HASH = environ.get("API_HASH", "1d693fcf3bfea119ca1d9057b08a4495")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7963199045:AAF56QGnfSRVlxz-Oh3O02Geo-cJ_hCUxGM")
+OWNER_ID = int(environ.get("OWNER_ID", "6004928770"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002541046049"))
+MONGO_DB_URI = environ.get("MONGO_DB_URI", "mongodb+srv://kalitgstringsession:kalitgstringsession@cluster0.rby7z6u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 PORT = int(environ.get('PORT', 8080))
-AUTH_CHANNELS = environ.get("AUTH_CHANNEL", "")
+AUTH_CHANNELS = environ.get("AUTH_CHANNEL", "-1002327045567")
 AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")]
